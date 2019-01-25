41bc506449d3f6730cfda5420e435aa3e8b3612e
boscocp <adalbertobosco@gmail.com>
2019-01-25 14:45:53 -0200

    Update AulaBuscas.java

--- AulaBuscas.java
+++ AulaBuscas.java
@@ -13,115 +13,22 @@ import java.util.List;
  * @author adalberto.bcpereira
  */
 public class AulaBuscas {
-    public static List<No> mapa = new ArrayList();
-    public static int linhas = 7;
-    public static int colunas = 6;
-    /**
-     * @param args the command line arguments
-     */
     
-    public static void criaMapa()
-    {
-        int contador = 0;
-        for (int i = 0; i<colunas; i++)
-        {
-            for (int j = 0; j<linhas; j++)
-            {
-                
-                No no = new No(contador);
-                mapa.add(no);
-                
-                contador++;
-            }
-        }
-    }
-    
-    
-    public static void configuraMapa()
-    {
-        for(No no: mapa)
-        {
-            no.vizinhos.addAll(acharCantos(no));
-            no.vizinhos.addAll(acharOrtogonais(no));
-        }
-    }
-    
-    public static List<No> acharCantos(No no)
-    {
-        int id = no.getId();
-        List<No> list = new ArrayList();
-        
-        //calcular linha
-        int linhaDoNo = (no.getId()/linhas)+1;
-        //calcula coluna
-        int colunaDoNo = (no.getId()%colunas)+1;
-            
-            
-        //pega canto superior esquerda
-        if (linhaDoNo > 1 && colunaDoNo > 1) {
-            list.add(mapa.get((id - colunas) - 1));
-        }
-        //pega canto superior direita
-        if (linhaDoNo > 1 && colunaDoNo < colunas) {
-            list.add(mapa.get((id - colunas) + 1));
-        }
-        
-        //pegar canto infoerior esquerdo
-        if (linhaDoNo < mapa.size() / linhas && colunaDoNo > 1) {
-            list.add(mapa.get((id + colunas) - 1));
-        }
-        //pegar canto inferior direito
-        if (linhaDoNo < mapa.size() / linhas && colunaDoNo < colunas) {
-            list.add(mapa.get((id + colunas) + 1));
-        }
-
-        return list;
-    }
-    
-    public static List<No> acharOrtogonais (No no)
-    {   
-        //calcular linha
-        int linhaDoNo = (no.getId()/linhas)+1;
-        //calcula coluna
-        int colunaDoNo = (no.getId()%colunas)+1;
-        List<No> list = new ArrayList();
-        int id = no.getId();
-        //pegar vizinho esquerdo
-        if (colunaDoNo > 1) {
-            list.add(mapa.get(id - 1));
-        }
-        //pegar vizinho direito
-       	if (colunaDoNo < colunas) {
-            list.add(mapa.get(id + 1));
-        }
-        //pegar vizinho cima
-        if (linhaDoNo > 1) {
-            list.add(mapa.get((id - linhas)+1));
-        }
-        //pegar vizinho baixo
-        if (linhaDoNo < mapa.size()/linhas) {
-            list.add(mapa.get(id + colunas));
-        }
-        
-        return list;
-    }
    
     
     public static void main(String[] args) {
         // TODO code application logic here
-        criaMapa();
-        configuraMapa();
-        
+        Mapa mapa = new Mapa(7,6);
         //System.out.println(mapa.get(0).vizinhos);
+        mapa.getMapa().get(12).setBloqueado(true);
+        mapa.getMapa().get(13).setBloqueado(true);
+        mapa.getMapa().get(14).setBloqueado(true);
+        mapa.getMapa().get(15).setBloqueado(true);
+        mapa.getMapa().get(16).setBloqueado(true);
         
-        for(No no: mapa){
-            System.out.println("Noh: "+ no.getId());
-        }
         System.out.println("--------------------------------");
-        for(No no: mapa.get(37).vizinhos){
-            System.out.println("Noh: "+ no.getId());
-        }
-        AEstrela.aEstrela(mapa.get(0), mapa.get(35));
+       
+        AEstrela.aEstrela(mapa.getMapa().get(0), mapa.getMapa().get(32), mapa);
         
         
     }
